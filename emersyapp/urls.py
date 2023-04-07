571aeca09f2c2069bf38b96acc4e1d19d541d18c
Makan DIANKA <makan.dianka@hotmail.com>
2023-04-07 14:46:16 +0200

    quickstart page

--- emersyapp/urls.py
+++ emersyapp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('quickstart', views.quickstart, name="index"),
 ]
--- emersyapp/views.py
+++ emersyapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "emersyapp/index.html")
+
+def quickstart(request):
+    return render(request, "emersyapp/quick.html")
